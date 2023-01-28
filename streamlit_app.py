ab951e3dc07bdffa4878c13f6a6fc2a3f9792a7e
Ivan Ivanov <101635841+vin-57@users.noreply.github.com>
2023-01-28 12:15:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,66 @@ HERE = Path(__file__).parent
 logger = logging.getLogger(__name__)
 
 
+def download_file(url, download_to: Path, expected_size=None):
+    # Don't download the file twice.
+    # (If possible, verify the download using the file length.)
+    if download_to.exists():
+        if expected_size:
+            if download_to.stat().st_size == expected_size:
+                return
+        else:
+            st.info(f"{url} is already downloaded.")
+            if not st.button("Download again?"):
+                return
+
+    download_to.parent.mkdir(parents=True, exist_ok=True)
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % url)
+        progress_bar = st.progress(0)
+        with open(download_to, "wb") as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning(
+                        "Загрузка %s... (%6.2f/%6.2f MB)"
+                        % (url, counter / MEGABYTES, length / MEGABYTES)
+                    )
+                    progress_bar.progress(min(counter / length, 1.0))
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
- # формируем web-страницу 
+
 def main():
-    st.header("SKLADRON demo")
+    st.header("SKLADRONE demo")
 
     pages = {
-        "Обнаружение объектов в реальном времени (sendrecv)": app_object_detection,
+        "Real time object detection (sendrecv)": app_object_detection,
     }
     page_titles = pages.keys()
 
     page_title = st.sidebar.selectbox(
-        "Текущий режим сканирования",
+        "Текущий режим",
         page_titles,
     )
     st.subheader(page_title)
@@ -51,7 +96,7 @@ def main():
     st.sidebar.markdown(
         """
 ---
-<a href="https://www.buymeacoffee.com/whitphx" target="_blank"><img src="skladron.png"  width="180" height="50" ></a>
+<a href="https://www.buymeacoffee.com/whitphx" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" width="180" height="50" ></a>
     """,  # noqa: E501
         unsafe_allow_html=True,
     )
@@ -130,9 +175,9 @@ def app_object_detection():
             confidence = detections[0, 0, i, 2]
 
             if confidence > confidence_threshold:
-                # извлечение индекса метки класса из `detections`,
-                # затем вычисляем (x, y)-координаты ограничивающей рамки
-                # объект
+                # extract the index of the class label from the `detections`,
+                # then compute the (x, y)-coordinates of the bounding box for
+                # the object
                 idx = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
@@ -140,7 +185,7 @@ def app_object_detection():
                 name = CLASSES[idx]
                 result.append(Detection(name=name, prob=float(confidence)))
 
-                # отображаем прогноз
+                # display the prediction
                 label = f"{name}: {round(confidence * 100, 2)}%"
                 cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
                 y = startY - 15 if startY - 15 > 15 else startY + 15
@@ -157,7 +202,7 @@ def app_object_detection():
 
     result_queue = (
         queue.Queue()
-    )  # TODO: объект общего состояния общего назначения может быть более полезным.
+    )  # TODO: A general-purpose shared state object may be more useful.
 
     def callback(frame: av.VideoFrame) -> av.VideoFrame:
         image = frame.to_ndarray(format="bgr24")
@@ -183,14 +228,14 @@ def app_object_detection():
         async_processing=True,
     )
 
-    if st.checkbox("Показать обнаруженные метки", value=True):
+    if st.checkbox("Show the detected labels", value=True):
         if webrtc_ctx.state.playing:
             labels_placeholder = st.empty()
-            # NOTE: Преобразование видео с обнаружением объектов и
-            # цикл, отображающий метки результатов, работает
-            # в разных потоках асинхронно.
-            # Затем визуализированные видеокадры и метки, отображаемые здесь
-            # строго не синхронизированы.
+            # NOTE: The video transformation with object detection and
+            # this loop displaying the result labels are running
+            # in different threads asynchronously.
+            # Then the rendered video frames and the labels displayed here
+            # are not strictly synchronized.
             while True:
                 try:
                     result = result_queue.get(timeout=1.0)
@@ -199,46 +244,56 @@ def app_object_detection():
                 labels_placeholder.table(result)
 
     st.markdown(
-        "Демо-версия проудкта использовует модель MobilenetSSD"
+        "This demo uses a model and code from "
+        "https://github.com/robmarkcole/object-detection-app. "
+        "Many thanks to the project."
     )
 
+ def callback(frame: av.VideoFrame) -> av.VideoFrame:
+        image = frame.to_ndarray(format="bgr24")
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+        )
+        net.setInput(blob)
+        detections = net.forward()
+        annotated_image, result = _annotate_image(image, detections)
 
-def app_streaming():
-    """Media streamings"""
-    MEDIAFILES = {
-        "big_buck_bunny_720p_2mb.mp4 (local)": {
-            "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4",  # noqa: E501
-            "local_file_path": HERE / "data/big_buck_bunny_720p_2mb.mp4",
-            "type": "video",
-        },
-        "big_buck_bunny_720p_10mb.mp4 (local)": {
-            "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4",  # noqa: E501
-            "local_file_path": HERE / "data/big_buck_bunny_720p_10mb.mp4",
-            "type": "video",
-        },
-        "file_example_MP3_700KB.mp3 (local)": {
-            "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",  # noqa: E501
-            "local_file_path": HERE / "data/file_example_MP3_700KB.mp3",
-            "type": "audio",
-        },
-        "file_example_MP3_5MG.mp3 (local)": {
-            "url": "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3",  # noqa: E501
-            "local_file_path": HERE / "data/file_example_MP3_5MG.mp3",
-            "type": "audio",
-        },
-        "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov": {
-            "url": "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov",
-            "type": "video",
-        },
-    }
-    media_file_label = st.radio(
-        "Select a media source to stream", tuple(MEDIAFILES.keys())
+        # NOTE: This `recv` method is called in another thread,
+        # so it must be thread-safe.
+        result_queue.put(result)  # TODO:
+
+        return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+
+    webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=callback,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
     )
-    media_file_info = MEDIAFILES[media_file_label]
-    if "local_file_path" in media_file_info:
-        download_file(media_file_info["url"], media_file_info["local_file_path"])
 
-        # создание видео-плеера
+    if st.checkbox("Show the detected labels", value=True):
+        if webrtc_ctx.state.playing:
+            labels_placeholder = st.empty()
+            # NOTE: The video transformation with object detection and
+            # this loop displaying the result labels are running
+            # in different threads asynchronously.
+            # Then the rendered video frames and the labels displayed here
+            # are not strictly synchronized.
+            while True:
+                try:
+                    result = result_queue.get(timeout=1.0)
+                except queue.Empty:
+                    result = None
+                labels_placeholder.table(result)
+
+    st.markdown(
+        "This demo uses a model and code from "
+        "https://github.com/robmarkcole/object-detection-app. "
+        "Many thanks to the project."
+    )
+
     def create_player():
         if "local_file_path" in media_file_info:
             return MediaPlayer(str(media_file_info["local_file_path"]))
@@ -315,6 +370,7 @@ def app_streaming():
         "https://github.com/aiortc/aiortc/blob/2362e6d1f0c730a0f8c387bbea76546775ad2fe8/examples/server/server.py#L34. "  # noqa: E501
         "Many thanks to the project."
     )
+
 
 if __name__ == "__main__":
     import os
