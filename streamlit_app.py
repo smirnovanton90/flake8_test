27c1a2fc94cdf4fa850cb5d2c62d647612596a2c
Ivan Ivanov <101635841+vin-57@users.noreply.github.com>
2023-01-28 11:57:35 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,61 +26,16 @@ HERE = Path(__file__).parent
 logger = logging.getLogger(__name__)
 
 
-def download_file(url, download_to: Path, expected_size=None):
-    # Don't download the file twice.
-    # (If possible, verify the download using the file length.)
-    if download_to.exists():
-        if expected_size:
-            if download_to.stat().st_size == expected_size:
-                return
-        else:
-            st.info(f"{url} is already downloaded.")
-            if not st.button("Download again?"):
-                return
-
-    download_to.parent.mkdir(parents=True, exist_ok=True)
-
-    # These are handles to two visual elements to animate.
-    weights_warning, progress_bar = None, None
-    try:
-        weights_warning = st.warning("Downloading %s..." % url)
-        progress_bar = st.progress(0)
-        with open(download_to, "wb") as output_file:
-            with urllib.request.urlopen(url) as response:
-                length = int(response.info()["Content-Length"])
-                counter = 0.0
-                MEGABYTES = 2.0 ** 20.0
-                while True:
-                    data = response.read(8192)
-                    if not data:
-                        break
-                    counter += len(data)
-                    output_file.write(data)
-
-                    # We perform animation by overwriting the elements.
-                    weights_warning.warning(
-                        "Downloading %s... (%6.2f/%6.2f MB)"
-                        % (url, counter / MEGABYTES, length / MEGABYTES)
-                    )
-                    progress_bar.progress(min(counter / length, 1.0))
-    # Finally, we remove these visual elements by calling .empty().
-    finally:
-        if weights_warning is not None:
-            weights_warning.empty()
-        if progress_bar is not None:
-            progress_bar.empty()
-
-
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-
+ # формируем web-страницу 
 def main():
     st.header("SKLADRON demo")
 
     pages = {
-        "Real time object detection (sendrecv)": app_object_detection,
+        "Обнаружение объектов в реальном времени (sendrecv)": app_object_detection,
     }
     page_titles = pages.keys()
 
@@ -96,7 +51,7 @@ def main():
     st.sidebar.markdown(
         """
 ---
-<a href="https://www.buymeacoffee.com/whitphx" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" width="180" height="50" ></a>
+<a href="https://www.buymeacoffee.com/whitphx" target="_blank"><img src="skladron.png"  width="180" height="50" ></a>
     """,  # noqa: E501
         unsafe_allow_html=True,
     )
@@ -175,9 +130,9 @@ def app_object_detection():
             confidence = detections[0, 0, i, 2]
 
             if confidence > confidence_threshold:
-                # extract the index of the class label from the `detections`,
-                # then compute the (x, y)-coordinates of the bounding box for
-                # the object
+                # извлечение индекса метки класса из `detections`,
+                # затем вычисляем (x, y)-координаты ограничивающей рамки
+                # объект
                 idx = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
@@ -185,7 +140,7 @@ def app_object_detection():
                 name = CLASSES[idx]
                 result.append(Detection(name=name, prob=float(confidence)))
 
-                # display the prediction
+                # отображаем прогноз
                 label = f"{name}: {round(confidence * 100, 2)}%"
                 cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
                 y = startY - 15 if startY - 15 > 15 else startY + 15
@@ -202,7 +157,7 @@ def app_object_detection():
 
     result_queue = (
         queue.Queue()
-    )  # TODO: A general-purpose shared state object may be more useful.
+    )  # TODO: объект общего состояния общего назначения может быть более полезным.
 
     def callback(frame: av.VideoFrame) -> av.VideoFrame:
         image = frame.to_ndarray(format="bgr24")
@@ -228,14 +183,14 @@ def app_object_detection():
         async_processing=True,
     )
 
-    if st.checkbox("Show the detected labels", value=True):
+    if st.checkbox("Показать обнаруженные метки", value=True):
         if webrtc_ctx.state.playing:
             labels_placeholder = st.empty()
-            # NOTE: The video transformation with object detection and
-            # this loop displaying the result labels are running
-            # in different threads asynchronously.
-            # Then the rendered video frames and the labels displayed here
-            # are not strictly synchronized.
+            # NOTE: Преобразование видео с обнаружением объектов и
+            # цикл, отображающий метки результатов, работает
+            # в разных потоках асинхронно.
+            # Затем визуализированные видеокадры и метки, отображаемые здесь
+            # строго не синхронизированы.
             while True:
                 try:
                     result = result_queue.get(timeout=1.0)
@@ -244,9 +199,7 @@ def app_object_detection():
                 labels_placeholder.table(result)
 
     st.markdown(
-        "This demo uses a model and code from "
-        "https://github.com/robmarkcole/object-detection-app. "
-        "Many thanks to the project."
+        "Демо-версия проудкта использовует модель MobilenetSSD"
     )
 
 
@@ -285,6 +238,7 @@ def app_streaming():
     if "local_file_path" in media_file_info:
         download_file(media_file_info["url"], media_file_info["local_file_path"])
 
+        # создание видео-плеера
     def create_player():
         if "local_file_path" in media_file_info:
             return MediaPlayer(str(media_file_info["local_file_path"]))
